bc3e26917c9b4b59d602717f0f0e365ebd707102
Dave Quinn <dquinn8@cogeco.ca>
2021-11-10 18:32:41 -0500

    added update post's title route

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -44,4 +44,42 @@ router.get('/:id', (req, res) => {
         });
 });
 
+//post a new post
+router.post('/', (req, res) => {
+    // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+    Post.create({
+            title: req.body.title,
+            post_url: req.body.post_url,
+            user_id: req.body.user_id
+        })
+        .then(dbPostData => res.json(dbPostData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+
+});
+
+// update a post title
+router.put('/:id', (req, res) => {
+    Post.update({
+            title: req.body.title
+        }, {
+            where: {
+                id: req.params.id
+            }
+        })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: 'No post found with this id' });
+                return;
+            }
+            res.json(dbPostData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
 module.exports = router;
