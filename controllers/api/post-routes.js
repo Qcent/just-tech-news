5cff5668346f4aa644397f3338d10e75b0c439fb
Dave Quinn <dquinn8@cogeco.ca>
2021-11-16 22:15:59 -0500

    single post now displayed with ability for logged in users to upvote and comment on posts.

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -88,15 +88,18 @@ router.post('/', (req, res) => {
 
 // PUT /api/posts/upvote
 router.put('/upvote', (req, res) => {
-
-    // create the vote
-    Vote.upvote(req.body, { vote })
-        .then(dbPostData => res.json(dbPostData))
-        .catch(err => {
-            console.log(err);
-            res.status(400).json(err);
-        });
+    // make sure the session exists first
+    if (req.session) {
+        // pass session id along with all destructured properties on req.body
+        Post.upvote({...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
+            .then(updatedVoteData => res.json(updatedVoteData))
+            .catch(err => {
+                console.log(err);
+                res.status(500).json(err);
+            });
+    }
 });
+
 
 // update a post title
 router.put('/:id', (req, res) => {
