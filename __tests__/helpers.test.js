699109502537130d21bd9165508db2505c86b37e
Dave Quinn <dquinn8@cogeco.ca>
2021-11-17 11:32:38 -0500

    added format_plural helper function and test

--- __tests__/helpers.test.js
+++ __tests__/helpers.test.js
@@ -1,7 +1,18 @@
-const { format_date } = require('../utils/helpers');
+const helper = require('../utils/helpers');
 
 test('format_date() returns a date string', () => {
     const date = new Date('2020-03-20 16:12:03');
 
-    expect(format_date(date)).toBe('3/20/2020');
+    expect(helper.format_date(date)).toBe('3/20/2020');
+});
+
+
+test('format_plural() returns correct pluralization', () => {
+    let one = ["Lion", 1];
+    let many = ["Lion", 2];
+    let none = ["Lion", 0];
+
+    expect(helper.format_plural(one)).toBe('Lion');
+    expect(helper.format_plural(many)).toBe('Lions');
+    expect(helper.format_plural(none)).toBe('Lions');
 });
--- utils/helpers.js
+++ utils/helpers.js
@@ -3,5 +3,9 @@ module.exports = {
         return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(
         date
       ).getFullYear()}`;
+    },
+    format_plural: ([word, num]) => {
+        if (num > 1 || num === 0) return word + 's';
+        return word;
     }
 }
